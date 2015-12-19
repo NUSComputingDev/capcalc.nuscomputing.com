3b089e2d52171fef6c456ecaeac76d3a280148f0
Nicholas Lum <nicholaslum444@hotmail.com>
2015-12-20 02:01:27 +0800

    Can now remove added modules.
    Calculation still not done

--- app/assets/javascripts/calculator.js
+++ app/assets/javascripts/calculator.js
@@ -2,13 +2,19 @@
  * Created by nick on 19/12/15.
  */
 function addNewModule() {
-    $("#modules").append($("#module-template .row").clone());
-    //$("body").append();
-    console.log("hello");
+    $("#modules").append($("#module-template .module").clone());
+    assignRemoveButton();
 }
 
 function saveDetails() {
     console.log("saving details");
+    // to save in cookies
+}
+
+function assignRemoveButton() {
+    $(".remove-button").on("click", function() {
+        $(this).closest(".module").remove();
+    });
 }
 
 $(function() {
@@ -16,7 +22,5 @@ $(function() {
     for (var i = 0; i < NUM_MODULES_TO_START_WITH; i++) {
         addNewModule();
     }
-});
-$(document).ready(function() {
-    console.log("asd");
+    assignRemoveButton()
 });
